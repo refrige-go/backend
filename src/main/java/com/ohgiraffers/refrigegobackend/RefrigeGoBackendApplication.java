9f4c82e171552c2c0c88b93e6838075b5f91fce2
dansun2 <dansunmusik7@gmail.com>
2025-05-18 16:26:43 +0900

    default

---
+++ src/main/java/com/ohgiraffers/refrigegobackend/RefrigeGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.refrigegobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigeGoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefrigeGoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/refrigegobackend/RefrigeGoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.refrigegobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigeGoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
