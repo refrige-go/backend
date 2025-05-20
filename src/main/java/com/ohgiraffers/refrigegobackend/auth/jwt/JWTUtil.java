472ceb2a28b2802e70b3460e32c3970c27417a1d
hodu <qp205@naver.com>
2025-05-20 18:29:06 +0900

    feat : JWT 의존성 수정

--- src/main/java/com/ohgiraffers/refrigegobackend/auth/jwt/JWTUtil.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/auth/jwt/JWTUtil.java
@@ -21,7 +21,6 @@ public class JWTUtil {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-
     public String getUsername(String token) {
 
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
