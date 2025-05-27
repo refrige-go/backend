a4d6f4648516db818f464c96d595263f6a7a579b
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-05-27 23:43:15 +0900

    fix: 레시피 북마크 정보 포함

--- src/main/java/com/ohgiraffers/refrigegobackend/recipe/controller/RecipeController.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/recipe/controller/RecipeController.java
@@ -7,7 +7,6 @@ import com.ohgiraffers.refrigegobackend.user.dto.CustomUserDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
