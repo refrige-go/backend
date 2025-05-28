4b7f705eba3e8186e17c4a50e61add3370b9243e
전우선 <jeonwooseon@naver.com>
2025-05-28 18:58:54 +0900

    코드 오류 수정

--- src/main/java/com/ohgiraffers/refrigegobackend/ingredient/infrastructure/repository/IngredientRepository.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/ingredient/infrastructure/repository/IngredientRepository.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.refrigegobackend.ingredient.infrastructure.repository;
 
 import com.ohgiraffers.refrigegobackend.ingredient.domain.Ingredient;
+import com.ohgiraffers.refrigegobackend.ingredient.domain.IngredientCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -20,7 +21,7 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     boolean existsByName(String name);
 
     // category로 재료 목록 조회 (null이나 empty는 전체 조회는 서비스에서 처리)
-    List<Ingredient> findByCategory(String category);
+    List<Ingredient> findByCategory(IngredientCategory category);
 
     @Query("SELECT DISTINCT i.category FROM Ingredient i ORDER BY i.category")
     List<String> findDistinctCategories();
