39889d9e584f63a209d16cad1db50a4ee572da5f
choe-yujin <dev.yujinchoe@gmail.com>
2025-05-29 20:14:53 +0900

    refactor: 프론트 코드와 연동되도록 수정

--- src/main/java/com/ohgiraffers/refrigegobackend/recommendation/dto/RecipeRecommendationRequestDto.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/recommendation/dto/RecipeRecommendationRequestDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class RecipeRecommendationRequestDto {
 
     /**
