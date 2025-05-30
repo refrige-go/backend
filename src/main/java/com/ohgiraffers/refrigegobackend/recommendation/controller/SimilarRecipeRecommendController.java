1494760baf1cc195b6de521c8b13e6c7a8bc7108
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-05-30 10:16:38 +0900

    feat: 비슷한 레시피 추천

---
+++ src/main/java/com/ohgiraffers/refrigegobackend/recommendation/controller/SimilarRecipeRecommendController.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.refrigegobackend.recommendation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recommendations")
+public class SimilarRecipeRecommendController {
+
+
+}
--- src/main/java/com/ohgiraffers/refrigegobackend/recommendation/service/WeatherRecommendationService.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/recommendation/service/WeatherRecommendationService.java
@@ -1,36 +1,58 @@
-package com.ohgiraffers.refrigegobackend.recommendation.service;
-
-import org.springframework.http.*;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-public class WeatherRecommendationService {
-
-    private final RestTemplate restTemplate;
-
-    public WeatherRecommendationService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public Map<String, Object> sendLocationToAIServer(Double latitude, Double longitude) {
-
-        String aiServerUrl = "http://localhost:8000/weather/recommend";
-
-        Map<String, Double> requestBody = new HashMap<>();
-        requestBody.put("latitude", latitude);
-        requestBody.put("longitude", longitude);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Map<String, Double>> requestEntity = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(aiServerUrl, requestEntity, Map.class);
-        return response.getBody();
-    }
-
-}
+//package com.ohgiraffers.refrigegobackend.recommendation.service;
+//
+//import com.ohgiraffers.refrigegobackend.recipe.infrastructure.repository.RecipeRepository;
+//import com.ohgiraffers.refrigegobackend.recommendation.dto.LocationToAiDTO;
+//import com.ohgiraffers.refrigegobackend.recommendation.dto.RecipeDTO;
+//import org.springframework.http.*;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class WeatherRecommendationService {
+//
+//    private final RestTemplate restTemplate;
+//    private final RecipeRepository recipeRepository;
+//
+//    public WeatherRecommendationService(RestTemplate restTemplate, RecipeRepository recipeRepository) {
+//        this.restTemplate = restTemplate;
+//        this.recipeRepository = recipeRepository;
+//    }
+//
+//    public Map<String, Object> sendLocationToAIServer(Double latitude, Double longitude) {
+//
+//        String aiServerUrl = "http://localhost:8000/weather/recommend/ai";
+//
+//        Map<String, Object> requestBody = new HashMap<>();
+//        requestBody.put("latitude", latitude);
+//        requestBody.put("longitude", longitude);
+//
+//        List<RecipeDTO> recipeList = fetchRecipesFromDB();
+//        requestBody.put("recipes", recipeList);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+//
+//        ResponseEntity<Map> response = restTemplate.postForEntity(aiServerUrl, requestEntity, Map.class);
+//        return response.getBody();
+//    }
+//
+//    private List<RecipeDTO> fetchRecipesFromDB() {
+//        List<RecipeDTO> recipes = recipeRepository.findAll().stream().map(recipe -> RecipeDTO.builder()
+//                .name(recipe.getRcpNm())
+//                .ingredients(recipe.getRcpPartsDtls())
+//                .category(recipe.getRcpCategory())
+//                .type(recipe.getCuisineType())
+//                .build()
+//        ).collect(Collectors.toList());
+//
+//        return recipes;
+//    }
+//}
