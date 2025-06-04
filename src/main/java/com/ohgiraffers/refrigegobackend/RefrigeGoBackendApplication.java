952f1991b8810eecb4feb04f5db927e660e6403b
dansun2 <dansunmusik7@gmail.com>
2025-06-04 15:19:04 +0900

    fix: mongoDB create 날짜 자동추가시 어노테이션 빌드 에러

--- src/main/java/com/ohgiraffers/refrigegobackend/RefrigeGoBackendApplication.java
+++ src/main/java/com/ohgiraffers/refrigegobackend/RefrigeGoBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableMongoAuditing
 @SpringBootApplication
 public class RefrigeGoBackendApplication {
 
